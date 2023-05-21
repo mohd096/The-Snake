2e16f62024dc3143e688b0f485b47b4f43bfd579
mohd <mohammed.mahfodh@gmail.com>
2023-05-21 15:16:45 +0300

    project 01 Snake Updated

--- script/main.js
+++ script/main.js
@@ -50,6 +50,7 @@ window.onload = function() {
 
 //starting from the corner of the page (width 500 height 500)/25*20 = 500
 function update() {
+    const sounds = document.querySelectorAll(".sound")
     context.fillStyle = "black" 
     context.fillRect(0, 0, border.width, border.height)
     
@@ -60,8 +61,10 @@ function update() {
     
     // To let the Snake consume the Food 
     if (snakeX == foodX && snakeY == foodY) {
-         drawScore(); 
+         drawScore();  // to add score each time the snake consume the food
         score++;
+        sounds[0].play()
+        
         
         snakeBody.push([foodX , foodY]) // After eating the food let the snake grow
         placeFood()
@@ -85,17 +88,20 @@ function update() {
 
     // GameOver Conditions
     // If the snake hit the wall (cols) alert Game Over
-    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
+    if ((snakeX < 0 || snakeX > cols*blockSize -25 || snakeY < 0 || snakeY > rows*blockSize -25) && gameOver == false) {
         gameOver = true
+        console.log("gameover")
         alert("Game Over")
-        return
+        window.location.reload()
+        return null
     }
     // If the snake hit it's body alert Game over
     for (let i = 0; i < snakeBody.length; i++) {
         if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
             gameOver = true
             alert("Game Over")
-            return
+            window.location.reload()
+            return null
         }
     }
 // Score system
@@ -111,43 +117,76 @@ function update() {
             }
             drawScore();   
 }
-// change Diraction if you priss arrow key it will call Change Diraction
+// change Diraction if you priss arrow key it will call Change Diraction       
+
 function changeDirection(event) {
     if (event.code == "ArrowUp" && velocityY !=1) { // To prevent the snake from going backword !=
         velocityX = 0
         velocityY = -1
+        console.log("arrowUp");
     }
     else if (event.code == "ArrowDown" && velocityY !=-1) {
         velocityX = 0
         velocityY = 1
+        console.log("arrowDown");
     }
     if (event.code == "ArrowLeft" && velocityX !=1) {
         velocityX = -1
         velocityY = 0
+        console.log("arrowLeft");
     }
     if (event.code == "ArrowRight" && velocityX !=-1) {
         velocityX = 1
         velocityY = 0
+        console.log("arrowRight");
     }
 
 }
+
 
 // To move the food in a random cordinate
 // Math.random is like this , return a number between (0-1)  multiplay by cols -> (0-19.9999) -> (0-19) multiply by blockSize * 25 
 function placeFood() {
     foodX = Math.floor(Math.random() * cols) * blockSize
     foodY = Math.floor(Math.random() * rows) * blockSize
-    
 }
 
-// context.font = "20px Arial";
-// context.fillStyle = "blue";
-// context.fillText("Score: ", 5, 20);
 
-// function drawScore() {
-//     context.font = "20px Arial";
-//     context.fillStyle = "red";
-//     context.filltext(score,  75, 20);
 
+
+
+
+
+// $(document).keydown(function(e) {
+//     if (e.keyCode in map) {
+//         map[e.keyCode] = true;
+//          if((map[37] && !map[38] && !map[39] && !map[40]) ||
+//             (!map[37] && map[38] && !map[39] && !map[40]) ||
+//             (!map[37] && !map[38] && map[39] && !map[40]) ||
+//             (!map[37] && !map[38] && !map[39] && map[40])) {
+//             if ((map[37]) && velocityX !=1) {
+//                 velocityX = -1
+//                 velocityY = 0
+//                 console.log("arrowLeft");
+//             } else if ((map[38]) && velocityY !=1) { 
+//                         velocityX = 0
+//                         velocityY = -1
+//                         console.log("arrowUp");
+//             } else if ((map[39])&& velocityX !=-1) {
+//                 velocityX = 1
+//                 velocityY = 0
+//                 console.log("arrowRight");
+//             } else if ((map[40]) && velocityY !=-1) {
+//                         velocityX = 0
+//                         velocityY = 1
+//                         console.log("arrowDown");
+//                     }
+//             }
 //         }
-// drawScore();
+//     }
+// ).keyup(function(e) {
+//     if (e.keyCode in map) {
+//         map[e.keyCode] = false;
+//     }
+// });
+
